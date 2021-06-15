80d3f1ba1c450048ccbae8e42e74aa5e1e9055ff
Aparna768213 <85796143+Aparna768213@users.noreply.github.com>
2021-06-15 12:11:53 +0530

    Add files via upload

---
+++ day4task.py
@@ -0,0 +1,17 @@
+a=b=c=120
+d,e,f=10,50,60
+print('The Division',a//d)
+print('The multiplication',b*e)
+print('The Addition',c+f)
+
+
+a='Aparna'
+print(a[ :3] + 'G' + a[4: ])
+
+
+a=106
+print(float(a))
+b=45.654
+print(int(b))
+
+
